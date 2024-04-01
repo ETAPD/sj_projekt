a7fcf0cd56a846c230e0dd1d5ccb4676773dbbc9
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-04-01 18:55:22 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="css/footer.css">
+<footer class="container-fluid bg-dark text-white  ">
+        <div class="row d-flex justify-content-center">
+            <div class="col-3">
+                <h4>O Nás:</h4>
+                <p>Digitálna brána do sveta umenia.</p>
+                <p>Spája 175 269 umeleckých diel z 21 galérií.</p>
+                <p>Objavujte, zdieľajte, prehliadajte.</p>
+            </div>
+            <div class="col-3">
+                <h4>Kontaktujte nás</h4>
+                <ul>
+                    <li><i class="fa fa-envelope" aria-hidden="true"><a
+                        href="mailto:tuan.anh.emil.pham.dac@student.ukf.sk">
+                        Mail</a></i></li>
+                    <li><i class="fa fa-phone" aria-hidden="true"><a
+                                href="tel:0909500600"> 0909500600</a></i></li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h4>Rýchle odkazy</h4>
+                <ul>
+                    <li><a href="/">Domov</a></li>
+                    <li><a href="fna.html">Q&A</a></li>
+                    <li><a href="kontakt.html">Kontakt</a></li>
+                    <li><a href="sluzby.html">Služby</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="row">
+            ⠀⠀⠀⠀ © 2021 Created and designed by PhamDac
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,29 @@
+
+    
+<link rel="stylesheet" href="css/nav.css">
+
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top container">
+            
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse middle" id="navbarNav">
+                <ul class="navbar-nav ">
+                    <li class="nav-item active">
+                        <a class="nav-link ps-3" href="index.php">Domov</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ps-3" href="contact.php">Kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ps-3 " href="about.php">F&A</a>
+                    </li>
+                    
+                </ul>
+            </div>
+    </nav> 
+</header>
+
+
