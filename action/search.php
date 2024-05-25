d809d064e8c8bc66ed54ed6ad9aa83f16314fa47
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-26 00:29:21 +0200

    search veci a js

---
+++ action/search.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../class/crud_post_opp.php';
+if (isset($_POST['query'])) {
+    $searchQuery = $_POST['query'];
+
+    $search = new Post();
+    $searchResults = $search->search_post($searchQuery);
+    $rows = array_chunk($searchResults, 3);
+
+    foreach ($rows as $row) {
+        echo '<div class="row">';
+        foreach ($row as $post): 
+            $backgroundImage = htmlspecialchars($post['title_img']);
+            //echo '<a href="post.php?id=' . $post['post_id'] . '">';
+            echo '<div class="col" style="border: 1px solid black; padding: 10px; margin: 10px; background-image: url(' . $backgroundImage . '); background-size: cover;" onclick="window.location.href=\'article.php?id=' . $post['post_id'] . '\'">';
+            echo '<h2 class="title">' . htmlspecialchars($post['title']) . '</h2>'; 
+            echo '<p class=" desription">' . (substr(strip_tags($post['content']), 0, 200))."..." .'</p>'; 
+            echo '</div>';
+            //echo '</a>';
+        endforeach;
+        echo '</div>';
+    }
+}
---
+++ js/search.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('#search').on('input', function(){
+        var searchQuery = $(this).val();
+        if (searchQuery === '') {
+            $.ajax({
+                url: 'action/default_search.php', 
+                type: 'POST',
+                success: function(data){
+                    $('#results').html(data);
+                }
+            });
+        } else {
+            $.ajax({
+                url: 'action/search.php',
+                type: 'POST',
+                data: {query: searchQuery},
+                success: function(data){
+                    $('#results').html(data);
+                }
+            });
+        }
+    }).trigger('input');
+});
+
+$(document).ready(function(){
+    $('.search').on('focus', function(){
+        if ($(this).val() === '') {
+            $(this).css('width', '420px');
+        }
+    });
+
+    $('.search').on('blur', function(){
+        if ($(this).val() === '') {
+            $(this).css('width', '40px');
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('.search').on('focus', function(){
+        $(this).attr('placeholder', 'Ako hrať....');
+        $(this).prev('.placeholder-image').remove(); // remove the image
+    });
+
+    $('.search').on('blur', function(){
+        if ($(this).val() === '') {
+            $(this).attr('placeholder', '');
+            $(this).before('<img src="media/img/lens.png" class="placeholder-image" />'); // add the image
+        }
+    });
+
+    $('.search').trigger('blur'); 
+});
