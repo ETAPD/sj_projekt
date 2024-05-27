9a0ea34426d818567b63f8832955c9a56732f096
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-27 17:23:32 +0200

    uprava session

--- class/session_oop.php
+++ class/session_oop.php
@@ -6,65 +6,15 @@ class Session {
 
     public function __construct() {
         
-        ini_set("session.use_only_cookies", 1);
-        ini_set("session.use_strict_mode", 1);
-
-        session_set_cookie_params([
-            
-            "lifetime" => $this->interval,
-            "domain" => "localhost",
-            "path" => "/",
-            "secure" => true,
-            "httponly" => true,
-        ]);
         session_start();
-    
-    }
+        if (isset($_SESSION["user_id"]) and (!isset($_SESSION["session_true"]))) {
+            $_SESSION["session_true"] = True;
+            session_regenerate_id();
 
-    public function handler() {
-
-        if (isset($_SESSION["user_id"])) {
-            
-            if (!isset($_SESSION["regeneration_time"])) {
-
-                $_SESSION["regeneration_time"] = time();
-                $this->login_session();
-    
-            } elseif (time() - $_SESSION["regeneration_time"] >= $this->interval) {
-                
-                $_SESSION["regeneration_time"] = time();
-                
-                
-            }
-        } else {
-
-            if (!isset($_SESSION["regeneration_time"])) {
-
-                $_SESSION["regeneration_time"] = time();
-                $this->guest_session();
-
-            } elseif (time() - $_SESSION["regeneration_time"] >= $this->interval) {
-                
-                $this->guest_session();
-                
-            }
-        }
-    }
-
-    private function guest_session() {
-
-        session_regenerate_id(true);
-        $_SESSION["regeneration_time"] = time();
-        
-    }
-
-    private function login_session() {
-
-        session_regenerate_id();
-        $user_id = $_SESSION["user_id"];
-        session_id(session_create_id() . "_" . $user_id);
-        $_SESSION["regeneration_time"] = time();
-
+        } elseif (!isset($_SESSION["user_id"]) and (!isset($_SESSION["session_faĺse"]))) {
+            $_SESSION["session_faĺse"] = False;
+            session_regenerate_id(true);
+        } 
     }
 
     public function logout() {
@@ -73,7 +23,7 @@ class Session {
         session_destroy();
         session_write_close();
         setcookie(session_name(), "", 0, "/");
-        $this->guest_session();
+        session_create_id();
 
     }
 }
