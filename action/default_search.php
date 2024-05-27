977d1bb65f0ce8a3c8a2254e37370a9a66f8ba0d
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-27 21:26:35 +0200

    uprava kontajneru

--- action/default_search.php
+++ action/default_search.php
@@ -6,15 +6,13 @@ $posts = $post->get_most_views();
 $rows = array_chunk($posts, 3);
 
 foreach ($rows as $row) {
-    echo '<div class="row">';
+    echo '<div class="row image-container">';
     foreach ($row as $post): 
         $backgroundImage = htmlspecialchars($post['title_img']);
-        //echo '<a href="post.php?id=' . $post['post_id'] . '">';
-        echo '<div class="col" style="border: 1px solid black; padding: 10px; margin: 10px; background-image: url(' . $backgroundImage . '); background-size: cover;" onclick="window.location.href=\'article.php?id=' . $post['post_id'] . '\'">';
+        echo '<div class="col" style=" padding: 10px; margin: 10px; background-image: url(' . $backgroundImage . '); background-size: cover;" onclick="window.location.href=\'article.php?id=' . $post['post_id'] . '\'">';
         echo '<h2 class="title">' . htmlspecialchars($post['title']) . '</h2>'; 
         echo '<p class=" desription">' . (substr(strip_tags($post['content']), 0, 200))."..." .'</p>'; 
         echo '</div>';
-        //echo '</a>';
     endforeach;
     echo '</div>';
 }
