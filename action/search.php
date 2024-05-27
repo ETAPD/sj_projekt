babd6a77354c02066ed26585ec16f8366948a36e
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-27 17:22:10 +0200

    uprava textarea

--- action/search.php
+++ action/search.php
@@ -8,11 +8,11 @@ if (isset($_POST['query'])) {
     $rows = array_chunk($searchResults, 3);
 
     foreach ($rows as $row) {
-        echo '<div class="row">';
+        echo '<div class="row image-container">';
         foreach ($row as $post): 
             $backgroundImage = htmlspecialchars($post['title_img']);
             //echo '<a href="post.php?id=' . $post['post_id'] . '">';
-            echo '<div class="col" style="border: 1px solid black; padding: 10px; margin: 10px; background-image: url(' . $backgroundImage . '); background-size: cover;" onclick="window.location.href=\'article.php?id=' . $post['post_id'] . '\'">';
+            echo '<div class="col" style=" padding: 10px; margin: 10px; background-image: url(' . $backgroundImage . '); background-size: cover;" onclick="window.location.href=\'article.php?id=' . $post['post_id'] . '\'">';
             echo '<h2 class="title">' . htmlspecialchars($post['title']) . '</h2>'; 
             echo '<p class=" desription">' . (substr(strip_tags($post['content']), 0, 200))."..." .'</p>'; 
             echo '</div>';
