6c2b7bb972ac6761ba522f7bd9877241c18b326b
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-27 21:26:49 +0200

    optimalizovanie

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
     require_once 'class/session_oop.php';
-    require_once 'action/session_act.php';
     require_once 'includes/head.php';
-    
-     
-    
-    
     
     
 ?>
@@ -17,60 +12,12 @@
         <img src="media/img/gamer_font.jpg" alt="" class="img-fluid">
     </div>
 
-    <div class="container">
-        <h1>Index</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quam nemo optio velit! Molestiae temporibus atque sed, voluptates culpa possimus dolorum praesentium accusamus. Quae, eveniet molestiae dolorem atque eum ut.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ea reprehenderit totam non dignissimos facere omnis iste! Ipsum omnis et quae odit cum reprehenderit, placeat temporibus debitis? Architecto, doloremque amet.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eos. Similique error quaerat accusantium iusto maiores magnam esse debitis, molestiae non ex quasi ducimus nam accusamus deleniti voluptatibus laboriosam quia!</p>   
+    <div class="container" id='results' >
     </div>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 1</h2>
-                <p>News content 1...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 2</h2>
-                <p>News content 2...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 1</h2>
-                <p>News content 1...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 2</h2>
-                <p>News content 2...</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 3</h2>
-                <p>News content 3...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 4</h2>
-                <p>News content 4...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 1</h2>
-                <p>News content 1...</p>
-            </div>
-            <div class="col" style="border: 1px solid black; padding: 10px; margin: 10px;">
-                <h2>News Title 2</h2>
-                <p>News content 2...</p>
-            </div>
-        </div>
-    </div>
-
-
-   
-
-
-
-
 
     <?php include('includes/footer.php') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/search.js"></script>
 </body>
 </html>
