2f5de3092bf8277548131598b551a76986b9a0ff
ETAPD <139513318+ETAPD@users.noreply.github.com>
2024-05-27 21:26:06 +0200

    vylepšenie kodu

--- action/manage_post_act.php
+++ action/manage_post_act.php
@@ -2,15 +2,13 @@
 $post = new Post();
 $posts = $post->get_all_post();
 $categories = $post->get_all_category();
+$stat = new Stat();
+
 
 
 if (isset($_GET['delete_id'])) {
     $post->delete_post($_GET['delete_id']);
     header("Location: manage_post.php");
-}
-
-if (isset($_GET['edit_id'])) {
-    $data_post = $post->read_post($_GET['edit_id']);
 }
 
 if (isset($_POST['edit'])) {
@@ -20,12 +18,37 @@ if (isset($_POST['edit'])) {
     $post_content = $_POST['content'];
     $post_category = $_POST['category'];
     $post->update_post($post_id, $post_title, $post_content, $post_category);
+    
+    if ($_FILES['title_img']['size'] > 0) {
+        
+        $file = $_FILES['title_img'];
+        $file_name = $file['name'];
+        $file_tmp_name = $file['tmp_name'];
+        $file_size = $file['size'];
+        $file_error = $file['error'];
+        $file_type = $file['type'];
+        $post->update_img($post_id, $file_name, $file_tmp_name, $file_size, $file_error, $file_type);
+    }
+    header("Location: manage_post.php");
 }
 
 if (isset($_POST['create'])) {
-
+   
     $post_title = $_POST['post_title'];
-    $post_content = $_POST['content'];
+    $post_content = $_POST['create-content'];
     $post_category = $_POST['category'];
-    $post->create_post($post_title, $post_content);
-}
+    $post_id=$post->create_post($post_title, $post_content, $post_category);
+    $stat_id = $stat->create_stat($post_id);
+    
+    if ($_FILES['title_img']['size'] > 0) {
+        
+        $file = $_FILES['title_img'];
+        $file_name = $file['name'];
+        $file_tmp_name = $file['tmp_name'];
+        $file_size = $file['size'];
+        $file_error = $file['error'];
+        $file_type = $file['type'];
+        $post->update_img($post_id, $file_name, $file_tmp_name, $file_size, $file_error, $file_type);
+    }
+    header("Location: manage_post.php");
+ }    
--- manage_post.php
+++ manage_post.php
@@ -1,110 +1,138 @@
 <?php
     require_once 'class/session_oop.php';
-    require_once 'action/session_act.php';
     require_once 'includes/head.php';
-    require_once 'class/manage_post_oop.php';
+    require_once 'class/crud_post_opp.php';
+    require_once 'class/stat_oop.php';
     require_once 'action/manage_post_act.php';
-    
 ?>
 <body>
 
     <?php require_once('includes/header.php') ?>
 
     <main>
-        
-        <?php if(isset($_GET['edit_id'])): ?>
-            
-        <section class="container-edit">
-            
-                
-            <h1>Upraviť príspevok</h1>
-
-            <form action="" method="post">
-                <div class="row">
-                <div class="col-md-6">
-                    <input type="hidden" name="edit_id" value="<?php echo $_GET['edit_id']; ?>">
-                    <div class="mb-3">
-                        <label for="post_title">Title</label><br>
-                        <input type="text" name="post_title" value="<?php echo $data_post[0]["title"];?>">
+      
+        <div class="modal fade" id="editPostModal" tabindex="-1" aria-labelledby="editPostModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editPostModalLabel">Upraviť článok</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="mb-3">
-                        <label for="Obsah">Content</label><br>
-                        <textarea name="content" id="content" cols="80" rows="10"><?php echo $data_post[0]["content"];?></textarea>
+                    <div class="modal-body">
+                        <form method="post" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <input type="hidden" name="edit_id">
+                                <label for="post_title" class="form-label">Názov</label>
+                                <input type="text" class="form-control" name="post_title" >
+                            </div>
+                            <div class="mb-3">
+                                <label for="Content" class="form-label">Obsah</label>
+                                <textarea class="form-control" name="content" id="content"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                
+                                <label for="Category" class="form-label">Kategórie</label>
+                                <select class="form-select" name="category" id="category">
+                                    <option value='0' selected>Vyberte kategóriu</option>
+                                    <?php foreach ($categories as $category): ?>
+                                        <option value="<?php echo $category["category_id"]  ?>" ><?php echo $category["name"] ?></option> 
+                                    <?php endforeach; ?>
+                                </select> 
+                                <input class="form-control" type="file" name="title_img">
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="return confirm('Ste si istý, že chcete zahodiť vaše zmeny?')">Zatvoriť</button>
+                            <button type="submit" class="btn btn-primary" name="edit" value="Upraviť" onclick="return confirm('Chcete upraviť svoje zmeny?')">Uložiť zmeny</button>
+                                </div>
+                        </form>
                     </div>
-                </div>
-                
-                <div class="col-md-6">
-                <div class="mb-3">
-                    <input type="submit" name="edit" value="Edit"> 
-                </div>
-                <div class="mb-3">
-                    <a href='manage_post.php?delete_id=<?php echo $row['edit_id']; ?>'>Delete</a></td>
-                </div>  
-                <div class="mb-3">
-                <select name="category" id="category">
-                    <option value="">Vyberte kategóriu</option>
                     
-                    <?php foreach ($categories as $category): ?>
-                        
-                        <option value="<?php echo $category["category_id"]  ?>" selected><?php echo $category["name"] ?></option> 
-                    <?php endforeach; ?>
-                        
-                    
-                </select>
-                </div>  
                 </div>
-                </div>
-            </form>
+            </div>
+        </div>
+           
                 
-            
               
+    
+
+    <div class="modal fade" id="createPostModal" tabindex="-1" aria-labelledby="createPostModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createPostModalLabel">Vytvoriť článok</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="post_title" class="form-label">Názov</label>
+                            <input type="text" name="post_title" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="content" class="form-label">Obsah</label>
+                            <textarea name="create-content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="category" class="form-label">Kategórie</label>
+                            <select name="category" id="category" class="form-select">
+                                <option value="0" selected>Vyberte kategóriu</option>
+                                <?php foreach ($categories as $category): ?>
+                                    <option value="<?php echo $category["category_id"]  ?>"><?php echo $category["name"] ?></option> 
+                                <?php endforeach; ?>
+                            </select> 
+                            <input type="file" name="title_img" class="form-control mt-3">
+                        </div>
+                        <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="return confirm('Ste si istý, že chcete zahodiť vaše zmeny?')">Zatvoriť</button>
+                                <button type="submit" class="btn btn-primary" name="create" value="Upraviť">Vytvoriť</button>
+                        </div>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+         
             
-        </section>
-        <?php elseif (isset($_GET['create-post'])): ?>  
+            <section class="container-manage-post">
+                <div style="table-head">
+                    <h1>Spravovať príspevky</h1>
+                    <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#createPostModal">Vytvoriť článok</button>
+                </div>
+                <table>
+                    <tr><th>Názov</th><th>Text</th><th>Vytvorené</th><th>Posledná úprava</th><th>Počet zobrazení</th><th>Upraviť / Vymazať</th></tr>
+                    
+                    <?php foreach ($posts as $row): ?>
+                        <tr>
+                            <td><?php echo $row['title']; ?></td>
+                            <td><?php echo substr($row['content'], 0, 255)."..."; ?></td>
+                            <td><?php echo $row['created_at']; ?></td>
+                            <td><?php echo $row['last_update']; ?></td>
+                            <td><?php echo $row['views']; ?></td>
+                            <td>
+                                <a href='#' class='edit-link' data-id='<?php echo $row['post_id']; ?>' data-row='<?php echo json_encode($row); ?>'>Upraviť</a>
+                                /<a href='manage_post.php?delete_id=<?php echo $row['post_id']; ?>'>Vymazať</a>
+                            </td>
+                        </tr>
+                    
+                    <?php endforeach; ?>
+                </table>
+            </section>
+       
             
-        <section class="container-create">
-        <form action="" method="post">
-                    
-                    <div class="mb-3">
-                        <label for="post_title">Title</label><br>
-                        <input type="text" name="post_title">
-                    </div>
-                    <div class="mb-3">
-                        <label for="Obsah">Content</label><br>
-                        <textarea name="content" id="content" cols="80" rows="10"></textarea></textarea>
-                    </div>
-                    <input type="submit" name="create" value="create">
-                    
-                </form>
-        </section> 
-        <?php else: ?>  
+        </main>
+        <?php include('includes/footer.php') ?>
+        <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+        <script>
+        CKEDITOR.replace('create-content', {
+            width: '100%',
+            height: 'auto'
+        });
+        </script>
         
-        <a href="manage_post.php?create-post">Create Post</a>
-        <section class="container-manage-post">
-            <table >
-            <h1>Spravovať príspevky</h1>
-            <tr><th>Title</th><th>Content</th><th>Created At</th><th>Last Update</th><th>Edit / Delete</th></tr>
-            
-            <?php foreach ($posts as $row): ?>
-                <tr>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row['content']; ?></td>
-                    <td><?php echo $row['created_at']; ?></td>
-                    <td><?php echo $row['last_update']; ?></td>
-                    <td><a href='manage_post.php?edit_id=<?php echo $row['post_id']; ?>'>Edit</a>/<a href='manage_post.php?delete_id=<?php echo $row['post_id']; ?>'>Delete</a></td>
-                    
-                </tr>
-            <?php endforeach; ?>
-            </table>
-        </section>
-        <?php endif; ?>
-    </main>
-    <?php include('includes/footer.php') ?>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>
-    tinymce.init({
-    selector: '#content'
-    });
-    </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="js/edit-post.js"></script>
+        
 </body>
 </html>
